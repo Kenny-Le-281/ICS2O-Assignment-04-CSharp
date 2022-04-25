7de4e2789acca821326589b5aa00b522c8c2b20a
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-25 17:03:21 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,43 @@
+// Created by: Kenny Le
+// Created on: Apr 2022
+//
+// This program calculates the pizza cost
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This program calculates the pizza cost
+        int large;
+        int extraLarge;
+        int oneTopping;
+        int twoTopping;
+        int threeTopping;
+        int fourTopping;
+        var pizzaSize = 0;
+        var pizzaTopping = 0;
+        
+        // input
+        Console.WriteLine("This calculates the price of your pizza order");
+
+        Console.WriteLine("");
+        Console.Write("Pick a pizza size ");
+        if (large )
+
+        // process and output
+        Console.WriteLine("");
+        if (guess == generatedNumber)
+        {
+            Console.WriteLine("You are correct!");
+        }
+
+        if (guess != generatedNumber)
+        {
+            Console.WriteLine("You are incorrect!");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
 }
